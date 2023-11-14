6aee1d642d453aa5ee1110c8854d49f33b21f742
kip9696 <kip9696@dynamicmedia.co.kr>
2023-11-14 15:40:17 +0900

    add refresh ssl

--- refresh_ssl.sh
+++ refresh_ssl.sh
@@ -6,5 +6,6 @@ source .env
 
 curl -L "${CMS}/get/ssl/chain/${LOCAL_IP}" > "./ssl/chain.pem";
 curl -L "${CMS}/get/ssl/key/${LOCAL_IP}" > "./ssl/key.pem";
+curl -L "${CMS}/get/ssl/ssl/${LOCAL_IP}" > "./ssl/ssl.pass";
 
 docker exec -it nginx /bin/bash -c "nginx -s reload"
