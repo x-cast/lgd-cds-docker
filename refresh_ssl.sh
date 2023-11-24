5137be6657df20863a383576a2dda45994e6124b
kip9696 <kip9696@dynamicmedia.co.kr>
2023-11-24 14:35:51 +0900

    add mkdir ssl

--- refresh_ssl.sh
+++ refresh_ssl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /data/lgd-cds-docker
 source .env
-
+mkdir ssl
 curl -L "${CMS}/get/ssl/chain/${LOCAL_IP}" > "./ssl/chain.pem";
 curl -L "${CMS}/get/ssl/key/${LOCAL_IP}" > "./ssl/key.pem";
 curl -L "${CMS}/get/ssl/ssl/${LOCAL_IP}" > "./ssl/ssl.pass";
--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 ### .env 파일은 수동으로 작성해 주세요 
 cd /data/lgd-cds-docker
 source .env
-
+mkdir ssl
 curl -L "${CMS}/get/ssl/chain/${LOCAL_IP}" > "./ssl/chain.pem";
 curl -L "${CMS}/get/ssl/key/${LOCAL_IP}" > "./ssl/key.pem";
 curl -L "${CMS}/get/ssl/ssl/${LOCAL_IP}" > "./ssl/ssl.pass";
