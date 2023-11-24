a77e7bc0d6e4668c1f13cde16664b0ab51af8263
kip9696 <kip9696@dynamicmedia.co.kr>
2023-11-24 14:39:46 +0900

    fix sh

--- start.sh
+++ start.sh
@@ -3,6 +3,8 @@
 cd /data/lgd-cds-docker
 source .env
 mkdir ssl
+mkdir logs
+
 curl -L "${CMS}/get/ssl/chain/${LOCAL_IP}" > "./ssl/chain.pem";
 curl -L "${CMS}/get/ssl/key/${LOCAL_IP}" > "./ssl/key.pem";
 curl -L "${CMS}/get/ssl/ssl/${LOCAL_IP}" > "./ssl/ssl.pass";
