c5c0e688d57da8c48eff31e11fe95b4fc35e25ee
kip9696 <kip9696@dynamicmedia.co.kr>
2023-11-24 14:34:28 +0900

    del ssl logs, fix sh

--- refresh_ssl.sh
+++ refresh_ssl.sh
@@ -6,4 +6,4 @@ curl -L "${CMS}/get/ssl/chain/${LOCAL_IP}" > "./ssl/chain.pem";
 curl -L "${CMS}/get/ssl/key/${LOCAL_IP}" > "./ssl/key.pem";
 curl -L "${CMS}/get/ssl/ssl/${LOCAL_IP}" > "./ssl/ssl.pass";
 
-docker exec -it nginx /bin/bash -c "nginx -s reload"
+docker exec -i nginx /bin/bash -c "nginx -s reload"
