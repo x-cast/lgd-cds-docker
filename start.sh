8647386520d7e74f48e5df175948810e7f83dfb4
kip9696 <kip9696@dynamicmedia.co.kr>
2023-12-13 17:27:19 +0900

    no tls logic add

--- start.sh
+++ start.sh
@@ -10,4 +10,11 @@ curl -L "${CMS}/get/ssl/chain/${LOCAL_IP}" > "./ssl/chain.pem";
 curl -L "${CMS}/get/ssl/key/${LOCAL_IP}" > "./ssl/key.pem";
 curl -L "${CMS}/get/ssl/ssl/${LOCAL_IP}" > "./ssl/ssl.pass";
 
+if[ -s "./ssl/chain.pem" ]; then
+    cp ./conf/vod-cds.conf ./vod-cds.conf
+else 
+    cp ./conf/vod-cds-notls.conf ./vod-cds.conf
+fi
+
+
 docker-compose up -d
