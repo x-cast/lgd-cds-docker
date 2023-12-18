f034186f8bcdc5d1e5ac3ce42a731bed01cd07e7
kip9696 <kip9696@dynamicmedia.co.kr>
2023-12-18 17:44:44 +0900

    fix manual

--- make-encrypt/1.sh
+++ make-encrypt/1.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-sudo dd if=/dev/urandom of=/dev/sdb bs=512 seek=4 count=16 ## /dev/sdb is the USB drive
-sudo dd if=/dev/sdb of=~/usb_secret.key bs=512 skip=4 count=16 ## 마찬가지로 확인
-xxd ~/usb_secret.key
+## 모든 명령어는 sudo su를 친상태에서 실행 [root 권한으로]
+## lsblk 입력해서 usb name 확인하자  ##
 
+sudo dd if=/dev/urandom of=${USB} bs=512 seek=4 count=16 ## /dev/sdb is the USB drive
+sudo dd if=${USB} of=~/usb_secret.key bs=512 skip=4 count=16 ## 마찬가지로 확인
+xxd ~/usb_secret.key
 
 blkid # 여기서 crypto device 확인
--- make-encrypt/2.sh
+++ make-encrypt/2.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-cryptsetup luksAddKey /dev/sda3 ~/usb_secret.key --key-slot 1 # /dev/sda3 ==> 1번에서 확인한 crypto device
+cryptsetup luksAddKey ${암호화된디바이스} ~/usb_secret.key --key-slot 1 # /dev/sda3 ==> 1번에서 blkid 를 통해 확인한 crypto device
 ## 암호 입력
 
-ln /dev/sdb /dev/usbdevice # /dev/sdb ==> 1번에서 확인한 USB device
+ln ${USB} /dev/usbdevice # /dev/sdb ==> 1번에서 확인한 USB device
 echo 'SUBSYSTEMS=="usb", DRIVERS=="usb", SYMLINK+="usbdevice%n"' > /etc/udev/rules.d/99-custom-usb.rules
 udevadm control --reload-rules
 
-echo '
-#!/bin/sh
+echo '#!/bin/sh
 TRUE=0
 FALSE=1
 
@@ -34,11 +33,10 @@ sleep 2
 ' > /usr/local/sbin/openluksdevices.sh
 chmod a+x /usr/local/sbin/openluksdevices.sh
 
-echo ,keyscript=/usr/local/sbin/openluksdevices.sh >> /etc/crypttab
+echo -n ',keyscript=/usr/local/sbin/openluksdevices.sh' >> /etc/crypttab ##이부분 들어가서 줄바꿈 되어있으면 위로 올리기
 
-echo CRYPTROOT=target=sda3_crypt,source=/dev/disk/by-uuid/${UUID!!} > /etc/initramfs-tools/conf.d/cryptroot #taget 확인하고, UUID 수정하자 
-echo '
-#!/bin/sh
+echo CRYPTROOT=target=${CRYPTO디바이스},source=/dev/disk/by-uuid/${UUID!!} > /etc/initramfs-tools/conf.d/cryptroot #taget 확인하고, UUID 수정하자 
+echo '#!/bin/sh
 PREREQ="udev"
 prereqs()
 {
